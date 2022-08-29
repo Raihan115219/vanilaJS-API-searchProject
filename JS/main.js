d98ee49c96e225e53f71e030a65e7895a4c6826e
Raihan115219 <aburaihan115219@gmail.com>
2022-08-29 12:23:03 +0600

    loader added and details page set

--- JS/main.js
+++ JS/main.js
@@ -1,42 +1,51 @@
 const gallery = document.getElementById("gallery");
 const searchBtn = document.getElementById("searchButton");
+const spinner = document.getElementById("spinner");
 
 // vent listenr create
 searchBtn.addEventListener("click", getResult);
 
 function getResult() {
-  let searchValue = document.getElementById("searchValue").value.trim();
-
+  gallery.innerHTML = `
+    <div class="text-center">
+          <div class="spinner-border text-danger" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+  `;
+  const searchTxt = document.getElementById("searchValue");
+  let searchValue = searchTxt.value.trim();
   fetch(
     ` https://pixabay.com/api/?key=24357815-a18ef174ca31639bec4066a17&q=${
       searchValue !== "" ? searchValue : "rose"
-    }?&image_type=photo`
+    }?&image_type=photo&per_page=30`
   )
     .then((response) => response.json())
     .then((data) => {
-      let html = "";
-      if (data !== "") {
-        data.hits.map((item, index) => {
-          console.log(item);
-          html += `
+      let result = "";
+      data.hits.map((item, index) => {
+        result += `
          <div class="col-lg-4">
-            <div class="card h-100 w-100 g-5 shadow-lg">
+            <div class="card h-100 w-100 g-5 shadow-lg" onclick="getDetails('${item.pageURL}')">
               <img
-                src=${item.webformatURL}
+                src=${item.largeImageURL}
                 class="card-img-top img-fluid h-100 w-100"
                 alt="..."
               />
-              <div>
-                <h5 class="card-title text-center">Picture : ${item.user}</h5>
+              <div class="text-center pb-2">
+                <h5 class="card-title text-center">User Name : ${item.user}</h5>
               </div>
             </div>
           </div>
         `;
-        });
-      } else {
-        html += "Sorry! We did not find any result";
-      }
-      gallery.innerHTML = html;
+        searchTxt.value = "";
+        gallery.innerHTML = result;
+      });
     });
 }
+
+const getDetails = (item) => {
+  window.location.href = `${item}`;
+};
+
 getResult();
